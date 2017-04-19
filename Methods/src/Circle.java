637b33e3131fca53a4d69f97d73ae2ad6aa5b3d6
erich <erich.flock@usp.br>
2017-04-19 14:59:47 +0200

    EvenOdd, Circle and Prime exercises 19-04-2017

--- Methods/src/Circle.java
+++ Methods/src/Circle.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 
 public class Circle {
-	//todo: add some methods to compute a circle's diameter, circumference and area.
-	//print some outputs to confirm you methods are working right.
+	// todo: add some methods to compute a circle's diameter, circumference and
+	// area.
+	// print some outputs to confirm you methods are working right.
+
+	
+	public static double diameter(double radious) {
+		return radious * 2;
+	}
+
+	public static double circumference(double diameter) {
+		return diameter * Math.PI;
+	}
+	
+	public static double area(double radious){
+		return Math.PI*(radious*radious);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the value of the radious: ");
+		int radious = scan.nextInt();
+		System.out.println("Diameter: "+diameter(radious));
+		System.out.println("Circumference: "+circumference(diameter(radious)));
+		System.out.println("Area: "+area(radious));
+	}
+	
 }
--- Methods/src/Prime.java
+++ Methods/src/Prime.java
@@ -3,8 +3,23 @@ import java.util.Scanner;
 
 public class Prime {
 	
+	public static boolean isPrime(int n) {
+	    for(int i=2;i<n;i++) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		int n = scan.nextInt();
+		
+		if(isPrime(n))
+			System.out.println("It is prime");
+		else
+			System.out.println("It is not prime");
 		
 		//todo: write a method which tells you if a given number is a prime or not
 		
