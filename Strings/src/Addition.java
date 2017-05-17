f2f2eb2f81c0d5ce5e24c0d44398057b817ca1d2
erich <erich.flock@usp.br>
2017-05-17 15:18:38 +0200

    Strings exercises. All tested and working well

--- Strings/src/Addition.java
+++ Strings/src/Addition.java
@@ -10,6 +10,17 @@ public class Addition {
 		//todo: Read integer values as input and add them until the user writes a 's'.
 		//Print the result.
 		
+		String input = scan.nextLine();
+		
+		int sum = 0;
+		while(!input.equals("s")){
+			sum += Integer.parseInt(input);
+			input = scan.nextLine();
+		}
+		
+		System.out.println("Sum: "+sum);
+		System.out.println("Tschuiss");
+		
 	}
 	
 }
