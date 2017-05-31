56e42c09f69d4297b215bbb22428f69483a5c075
erich <erich.flock@usp.br>
2017-05-31 13:28:29 +0200

    OOP1 exercise

--- OOP1/src/Lecture.java
+++ OOP1/src/Lecture.java
@@ -5,6 +5,27 @@ public class Lecture {
 	
 	//todo: write a constructor which already takes a name and credits
 	
+	public Lecture(String lectureName, double lectureCredits){
+		name = lectureName;
+		credits = lectureCredits;
+	}
+	
 	//todo: write getter and setter methods
-
+	
+	public void setName(String lectureName){
+		name = lectureName;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setCredits(double lectureCredits){
+		credits = lectureCredits;
+	}
+	
+	public double getCredits(){
+		return credits;
+	}
+	
 }
